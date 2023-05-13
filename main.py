2139b2ec07deab36099774ad68a709c869c1b7ab
Mario de León <46909802+DavidDLM@users.noreply.github.com>
2023-05-13 00:03:13 -0600

    Added files via upload

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 from parser_1 import *
 
 filename = "yap3.yalp"
-yap_file = 'YAPar/' + filename
+yapFilename = 'YAPar/' + filename
 
-yapar = Parser(yap_file)
+yapar = Parser(yapFilename)
 
 yapar.compiler()
